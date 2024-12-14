9def14b49cd3795cfd370b7b425e77234607a4c1
Scarxhold <118264138+Scarxhold@users.noreply.github.com>
2024-12-14 11:15:40 +0200

    Add files via upload

---
+++ Controllers/SymmetryController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using test2.Models;
+
+namespace test2.Controllers
+{
+    public class SymmetryController : Controller
+    {
+        
+            public IActionResult Index()
+            {
+                return View(new SymmetryViewModel());
+            }
+
+            [HttpPost]
+            public IActionResult CheckSymmetry(SymmetryViewModel model)
+            {
+                if (ModelState.IsValid)
+                {
+                    model.IsSymmetrical = IsSymmetrical(model.Number);
+                }
+                return View("Index", model);
+            }
+
+            private bool IsSymmetrical(int number)
+            {
+                string strNum = number.ToString();
+                string reversed = new string(strNum.Reverse().ToArray());
+                return strNum == reversed;
+            }
+        
+    }
+}
---
+++ Models/SymmetryViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace test2.Models
+{
+    public class SymmetryViewModel
+    {
+        [Required]
+        public int Number { get; set; }
+        public bool? IsSymmetrical { get; set; }
+    }
+}
